2a0d429ced90c797de83c69fa61b9da1490a1d12
guramijajanidze <gurami.jajanidze@gmail.com>
2024-05-08 12:49:17 +0400

    Corrected the mistkaes in the texts

---
+++ quizA1.1-1.jsx
@@ -0,0 +1,18 @@
+function Card () {
+    return (
+        <div>
+            <div class="word"></div>
+            <div class="word"></div>
+            <div class="word"></div>
+            <div class="word"></div>
+            <div class="word"></div>
+            <div class="word"></div>
+            <div class="word"></div>
+            <div class="word"></div>
+            <div class="word"></div>
+            <div class="word"></div>
+        </div>
+    )
+}
+
+export default Card
